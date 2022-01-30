2cad05bdf1c6e8561706888eb8aa6d0f66656a04
Turo-Rudi <jozsa.andrea@gmail.com>
2022-01-30 21:56:24 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+  strings: ["Frontend", "Backend", "Full-Stack", "Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
